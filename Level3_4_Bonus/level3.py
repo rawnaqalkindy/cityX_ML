1f066da1df5b06fe33133c446fdb53158e06d2dc
Rawnaq El Kindy <72880663+rawnaqalkindy@users.noreply.github.com>
2025-03-25 16:01:46 +0400

    old level3.py

--- Level3_4_Bonus/level3.py
+++ Level3_4_Bonus/level3.py
@@ -4,22 +4,11 @@ import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
 
-from model import assign_severity
-
 base_path = "/app/"
 csv_path = os.path.join(base_path, "Competition_Dataset.csv")
 
-def severity_to_icon_color(severity):
-    color_map = {
-        1: "yellow",
-        2: "lightorange",
-        3: "orange",
-        4: "red",
-        5: "darkred"
-    }
-    return color_map.get(severity, "gray")
-
 def create_geo_map():
+
     df = pd.read_csv(csv_path)
     df.columns = df.columns.str.strip().str.lower()
 
@@ -30,42 +19,37 @@ def create_geo_map():
     df.rename(columns={
         "temp_long": "longitude (x)",
         "temp_lat": "latitude (y)"
-    }, inplace=True)
+    }, inplace=True) # Rename due to opposite values in original dataset
+
 
     df["latitude (y)"] = pd.to_numeric(df["latitude (y)"])
     df["longitude (x)"] = pd.to_numeric(df["longitude (x)"])
-    df.dropna(subset=["latitude (y)", "longitude (x)"], inplace=True)
+    df.dropna(subset=["latitude (y)", "longitude (x)"], inplace=True) 
 
     if df.empty:
         print("No valid rows. Nothing to display.")
         return "<h3>No valid data for the map.</h3>"
 
-    # if len(df) > 300:
-    #     df = df.sample(n=300, random_state=42)
+    if len(df) > 100:
+        df = df.sample(n=100, random_state=42)     # Sample data for quicker results, comment out for testing full dataset
 
     sanfran_map = folium.Map(
         location=[df["latitude (y)"].median(), df["longitude (x)"].median()],
         zoom_start=12,
         tiles="CartoDB positron"
     )
-
     incidents = MarkerCluster().add_to(sanfran_map)
-
+    lat_col = "latitude (y)"
+    lon_col = "longitude (x)"
     if "category" in df.columns:
         labels = df["category"].tolist()
     else:
         labels = ["Crime" for _ in range(len(df))]
 
-    lat_col = "latitude (y)"
-    lon_col = "longitude (x)"
-    
-    for lat, lng, category in zip(df[lat_col], df[lon_col], labels):
-        sev = assign_severity(category)
-        icon_color = severity_to_icon_color(sev)
-
+    for lat, lng, label in zip(df[lat_col], df[lon_col], labels):
         folium.Marker(
             location=[lat, lng],
-            icon=folium.Icon(color=icon_color, icon='info-sign') 
+            popup=label
         ).add_to(incidents)
 
     return sanfran_map._repr_html_()
