8c37e646f178a675e4fe3d0ceaf6100bb0ffd557
Rawnaq El Kindy <72880663+rawnaqalkindy@users.noreply.github.com>
2025-03-25 15:32:59 +0400

    Update level3.py

--- Level3_4_Bonus/level3.py
+++ Level3_4_Bonus/level3.py
@@ -8,7 +8,6 @@ base_path = "/app/"
 csv_path = os.path.join(base_path, "Competition_Dataset.csv")
 
 def create_geo_map():
-
     df = pd.read_csv(csv_path)
     df.columns = df.columns.str.strip().str.lower()
 
@@ -19,40 +18,54 @@ def create_geo_map():
     df.rename(columns={
         "temp_long": "longitude (x)",
         "temp_lat": "latitude (y)"
-    }, inplace=True) # Rename due to opposite values in original dataset
-
+    }, inplace=True)
 
     df["latitude (y)"] = pd.to_numeric(df["latitude (y)"])
     df["longitude (x)"] = pd.to_numeric(df["longitude (x)"])
-    df.dropna(subset=["latitude (y)", "longitude (x)"], inplace=True) 
+    df.dropna(subset=["latitude (y)", "longitude (x)"], inplace=True)
 
     if df.empty:
         print("No valid rows. Nothing to display.")
         return "<h3>No valid data for the map.</h3>"
 
-    if len(df) > 100:
-        df = df.sample(n=100, random_state=42)     # Sample data for quicker results, comment out for testing full dataset
+
+    # if len(df) > 300:
+    #     df = df.sample(n=300, random_state=42)
+
 
     sanfran_map = folium.Map(
         location=[df["latitude (y)"].median(), df["longitude (x)"].median()],
         zoom_start=12,
         tiles="CartoDB positron"
     )
+
+
     incidents = MarkerCluster().add_to(sanfran_map)
-    lat_col = "latitude (y)"
-    lon_col = "longitude (x)"
+
+
     if "category" in df.columns:
         labels = df["category"].tolist()
     else:
         labels = ["Crime" for _ in range(len(df))]
 
-    for lat, lng, label in zip(df[lat_col], df[lon_col], labels):
-        folium.Marker(
+    lat_col = "latitude (y)"
+    lon_col = "longitude (x)"
+
+    for lat, lng, category in zip(df[lat_col], df[lon_col], labels):
+
+        sev = assign_severity(category)
+        color_hex = severity_to_color(sev)
+
+
+        folium.CircleMarker(
             location=[lat, lng],
-            popup=label
+            radius=5,                   # marker size
+            popup=f"{category} (Severity {sev})",
+            color=color_hex,            # outline color
+            fill=True,
+            fill_color=color_hex,       # fill color
+            fill_opacity=0.8
         ).add_to(incidents)
 
+
     return sanfran_map._repr_html_()
-
-
-
