b5cf335772d9b3f4ee87d9373fe6fe57f0b54ea8
Rawnaq El Kindy <72880663+rawnaqalkindy@users.noreply.github.com>
2025-03-25 15:21:50 +0400

    Update shrink_dataset.py

--- Level3_4_Bonus/shrink_dataset.py
+++ Level3_4_Bonus/shrink_dataset.py
@@ -4,4 +4,4 @@ df = pd.read_csv("Competition_Dataset.csv")
 df_small = df.sample(n=300, random_state=42)
 df_small.to_csv("Competition_Dataset_small.csv", index=False)
 
-print("Created Competition_Dataset_small.csv with 300 rows.")
+print("Created Competition_Dataset_small.csv with 300 rows.") # Shrink dataset to random 300 entries for deployment purposes
