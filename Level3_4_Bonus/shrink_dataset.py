b92027a22b5add2855ee73c16a32d7953d87f082
rawnaqalkindy <72880663+rawnaqalkindy@users.noreply.github.com>
2025-03-25 15:14:10 +0400

    Added shrink_dataset.py file

---
+++ Level3_4_Bonus/shrink_dataset.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("Competition_Dataset.csv")
+df_small = df.sample(n=300, random_state=42)
+df_small.to_csv("Competition_Dataset_small.csv", index=False)
+
+print("Created Competition_Dataset_small.csv with 300 rows.")
